277b3100d170f4408858bcb6df17b789d907c9be
leowlf <leonard.wolff75@gmail.com>
2025-12-08 17:32:59 +0100

    Initial Commit

---
+++ NaviProgress/NaviProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviProgressApp.swift
+//  NaviProgress
+//
+//  Created by Wolff on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NaviProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
